594f81160345bb6350fb6f5594b8f92b1769816b
Esmaeill <liyanboy74@gmail.com>
2022-09-25 08:12:48 +0330

    add some comment

--- prog.c
+++ prog.c
@@ -227,11 +227,11 @@ void main(void)
   // USART Transmitter: On
   // USART Mode: Asynchronous
   // USART Baud Rate: 9600
-  // UCSRA=(0<<RXC) | (0<<TXC) | (0<<UDRE) | (0<<FE) | (0<<DOR) | (0<<UPE) | (0<<U2X) | (0<<MPCM);
-  // UCSRB=(0<<RXCIE) | (0<<TXCIE) | (0<<UDRIE) | (1<<RXEN) | (1<<TXEN) | (0<<UCSZ2) | (0<<RXB8) | (0<<TXB8);
-  // UCSRC=(1<<URSEL) | (0<<UMSEL) | (0<<UPM1) | (0<<UPM0) | (0<<USBS) | (1<<UCSZ1) | (1<<UCSZ0) | (0<<UCPOL);
-  // UBRRH=0x00;
-  // UBRRL=0x33;
+  UCSRA=(0<<RXC) | (0<<TXC) | (0<<UDRE) | (0<<FE) | (0<<DOR) | (0<<UPE) | (0<<U2X) | (0<<MPCM);
+  UCSRB=(0<<RXCIE) | (0<<TXCIE) | (0<<UDRIE) | (1<<RXEN) | (1<<TXEN) | (0<<UCSZ2) | (0<<RXB8) | (0<<TXB8);
+  UCSRC=(1<<URSEL) | (0<<UMSEL) | (0<<UPM1) | (0<<UPM0) | (0<<USBS) | (1<<UCSZ1) | (1<<UCSZ0) | (0<<UCPOL);
+  UBRRH=0x00;
+  UBRRL=0x33;
 
 
   // Global enable interrupts
@@ -241,8 +241,8 @@ void main(void)
   
   while (1)
   {
-    set_max_pwm_ds(get_adc(0)/2.55);
-    set_wave_shift_speed(get_adc(1)/8.5);
+    set_max_pwm_ds(get_adc(0)/2.55);// Up to 100
+    set_wave_shift_speed(get_adc(1)/8.5);//Up to 30
     // printf("Shift:%02d \tPWM_Div:%0.2f\r\n",f_shift,pwm_div);
     // delay_ms(500);
   }
