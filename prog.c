9b87d83b3db197f15ddee3a5d45ae42b8e49f32f
Esmaeill <liyanboy74@gmail.com>
2022-09-30 11:48:38 +0330

    add some space

--- prog.c
+++ prog.c
@@ -67,15 +67,15 @@ void timer_pwm_stop();
 // of the AD conversion result
 unsigned char read_adc(unsigned char adc_input)
 {
-ADMUX=adc_input | ADC_VREF_TYPE;
-// Delay needed for the stabilization of the ADC input voltage
-delay_us(10);
-// Start the AD conversion
-ADCSRA|=(1<<ADSC);
-// Wait for the AD conversion to complete
-while ((ADCSRA & (1<<ADIF))==0);
-ADCSRA|=(1<<ADIF);
-return ADCH;
+  ADMUX=adc_input | ADC_VREF_TYPE;
+  // Delay needed for the stabilization of the ADC input voltage
+  delay_us(10);
+  // Start the AD conversion
+  ADCSRA|=(1<<ADSC);
+  // Wait for the AD conversion to complete
+  while ((ADCSRA & (1<<ADIF))==0);
+  ADCSRA|=(1<<ADIF);
+  return ADCH;
 }
 
 // External Interrupt 0 service routine
